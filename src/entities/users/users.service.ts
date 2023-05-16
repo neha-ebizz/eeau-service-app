0bfacec6350194a4071cfc75c6bccf31cc524619
neha-ebizz <neha@ebizzinfotech.com>
2023-05-16 09:53:23 +0530

    Added auth guard

--- src/entities/users/users.service.ts
+++ src/entities/users/users.service.ts
@@ -30,7 +30,7 @@ export class UsersService {
     const payload = {
       id: createdRecord.id,
     };
-    var access_token = await this.jwtService.signAsync(payload);
+    const access_token = await this.jwtService.signAsync(payload);
 
     return access_token;
   };
@@ -51,7 +51,7 @@ export class UsersService {
     const payload = {
       id: createdRecord.id,
     };
-    var access_token = await this.jwtService.signAsync(payload);
+    const access_token = await this.jwtService.signAsync(payload);
 
     return access_token;
   };
